f16b4fb5861d5d5d2dec23f6b74b273681b73bf9
alvarogonmar <alvarogonmar0@gmail.com>
2024-09-30 22:55:24 -0600

    Adding error when a color doesn't exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import Values from "values.js";
 import FormColor from "./components/FormColor";
-
 import "./App.css";
 
 function App() {
-  const [list, setlist] = useState(new Values("red").all(10));
+  const [list, setList] = useState(new Values("red").all(10));
 
   console.log(list);
 
   return (
     <div className="App">
       <h2>test component</h2>
-      <FormColor></FormColor>
+      <FormColor setList={setList} />
     </div>
   );
 }
--- src/components/FormColor.tsx
+++ src/components/FormColor.tsx
@@ -1,22 +1,39 @@
 import { useState } from "react";
+import Values from "values.js";
 
-const FormColor = () => {
+interface FormColorProps {
+  setList: React.Dispatch<React.SetStateAction<any>>; // Cambia 'any' por el tipo adecuado, si es necesario
+}
+const FormColor: React.FC<FormColorProps> = ({ setList }) => {
   const [color, setColor] = useState("blue");
+  const [error, setError] = useState(false);
 
   const handleGenerator = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      setError(false);
+      //console.log(colors);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
     console.log(color);
   };
 
   return (
-    <form onSubmit={handleGenerator}>
-      <input
-        type="text"
-        placeholder="#fff"
-        onChange={(e) => setColor(e.target.value)}
-      />
-      <input type="submit" value="Generate" />
-    </form>
+    <>
+      <form onSubmit={handleGenerator}>
+        <input
+          type="text"
+          placeholder="#fff"
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input type="submit" value="Generate" />
+      </form>
+      {error ? <p className="error">Doesn't exist that color...</p> : null}
+    </>
   );
 };
 
